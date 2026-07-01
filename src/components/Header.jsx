d621656ce11e27c385b84163bd4780def058c722
daviderocco85 <roccodavide85@gmail.com>
2026-07-02 01:07:10 +0200

    feat: update Header.jsx component with logo and navigation structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,31 @@
-export const Header = () => (<></>);
+import './Header.css';
+
+const Logo = () => <img className='logo' src="img/dc-logo.png" alt='Logo' />
+
+const Nav = () => (
+    <nav>
+        <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Tv</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Collectibles</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Fans</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Shop</a></li>
+        </ul>
+    </nav>
+);
+
+export const Header = () => (
+    <header>
+        <div className="container">
+            <Logo />
+            <Nav />
+        </div>
+    </header>
+
+
+);
