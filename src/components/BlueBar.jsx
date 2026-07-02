ffd997b201195c6da276eaad826592ed3f20716f
daviderocco85 <roccodavide85@gmail.com>
2026-07-02 12:43:59 +0200

    refactor: replace BlueBar item components with NavBlue list-based structure

--- src/components/BlueBar.jsx
+++ src/components/BlueBar.jsx
@@ -1,50 +1,40 @@
 import "./BlueBar.css";
 
-const Item1 = () => (
-    <div className="item">
-        <img src="img/buy-comics-digital-comics.png" alt="" />
-        <span>DIGITAL COMICS</span>
-    </div>
+
+
+const NavBlue = () => (
+    <nav>
+        <ul>
+            <li>
+                <img src="img/buy-comics-digital-comics.png" alt="" />
+                <a href="#"><span>DIGITAL COMICS</span></a>
+            </li>
+            <li>
+                <img src="img/buy-comics-merchandise.png" alt="" />
+                <a href="#"><span>DC MERCHANDISE</span></a>
+            </li>
+            <li>
+                <img src="img/buy-comics-subscriptions.png" alt="" />
+                <a href="#"><span>SUBSCRIPTIONS</span></a>
+            </li>
+            <li>
+                <img src="img/buy-comics-shop-locator.png" alt="" />
+                <a href="#"><span>COMIC SHOP LOCATOR</span></a>
+            </li>
+            <li>
+                <img src="img/buy-dc-power-visa.svg" className="power-visa" alt="" />
+                <a href="#"> <span>DC POWER VISA</span></a>
+            </li>
+        </ul>
+    </nav>
 );
 
-const Item2 = () => (
-    <div className="item">
-        <img src="img/buy-comics-merchandise.png" alt="" />
-        <span>DC MERCHANDISE</span>
-    </div>
-);
+export const BlueBar = () => (
 
-const Item3 = () => (
-    <div className="item">
-        <img src="img/buy-comics-subscriptions.png" alt="" />
-        <span>SUBSCRIPTIONS</span>
-    </div>
-);
+    <section className="blue-bar">
+        <div className="container blue-content">
+            <NavBlue />
+        </div>
+    </section>
 
-const Item4 = () => (
-    <div className="item">
-        <img src="img/buy-comics-shop-locator.png" alt="" />
-        <span>COMIC SHOP LOCATOR</span>
-    </div>
 );
-
-const Item5 = () => (
-    <div className="item">
-        <img src="img/buy-dc-power-visa.svg" className="power-visa" alt="" />
-        <span>DC POWER VISA</span>
-    </div>
-);
-
-export const BlueBar = () => {
-    return (
-        <section className="blue-bar">
-            <div className="container blue-content">
-                <Item1 />
-                <Item2 />
-                <Item3 />
-                <Item4 />
-                <Item5 />
-            </div>
-        </section>
-    );
-};
