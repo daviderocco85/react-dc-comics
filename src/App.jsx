b3c8e2d42c45961298f1fa62b7eb49bf286fcb0c
daviderocco85 <roccodavide85@gmail.com>
2026-07-02 13:54:33 +0200

    refactor: remove unused useState import from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import './App.css';
 import { Header } from './components/Header';
 import { Main } from './components/Main';
 import { BlueBar } from './components/BlueBar';
 import { Footer } from './components/Footer';
-import './App.css';
+
 
 const App = () => {
 
