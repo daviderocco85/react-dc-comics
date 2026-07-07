0827f3f67c5b15bb9ef7f2a0249844d6ddb3f538
daviderocco85 <roccodavide85@gmail.com>
2026-07-07 14:01:26 +0200

    feat: add headerNavLinks.js data source for Header component

---
+++ src/data/headerNavLinks.js
@@ -0,0 +1,62 @@
+export const headerNavLinks = [
+    {
+        id: 1,
+        href: '#',
+        content: 'Characters',
+        active: false
+    },
+    {
+        id: 2,
+        href: '#',
+        content: 'Comics',
+        active: true
+    },
+    {
+        id: 3,
+        href: '#',
+        content: 'Movies',
+        active: false
+    },
+    {
+        id: 4,
+        href: '#',
+        content: 'Tv',
+        active: false
+    },
+    {
+        id: 5,
+        href: '#',
+        content: 'Games',
+        active: false
+    },
+    {
+        id: 6,
+        href: '#',
+        content: 'Collectibles',
+        active: false
+    },
+    {
+        id: 7,
+        href: '#',
+        content: 'Videos',
+        active: false
+    },
+    {
+        id: 8,
+        href: '#',
+        content: 'Fans',
+        active: false
+    },
+    {
+        id: 9,
+        href: '#',
+        content: 'News',
+        active: false
+    },
+    {
+        id: 10,
+        href: '#',
+        content: 'Shop',
+        active: false
+    },
+]
