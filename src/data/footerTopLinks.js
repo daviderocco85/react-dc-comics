4cfd1092c6686b98fbb5cb5eefee8fb944537467
daviderocco85 <roccodavide85@gmail.com>
2026-07-07 13:57:11 +0200

    feat: add footerTopLinks.js data source for Footer component

---
+++ src/data/footerTopLinks.js
@@ -0,0 +1,58 @@
+export const footerTopLinks = [
+    {
+        id: 1,
+        className: 'dccomics',
+        title: 'DC Comics',
+        links: [
+            { id: 1, href: '#', content: 'Characters' },
+            { id: 2, href: '#', content: 'Comics' },
+            { id: 3, href: '#', content: 'Movies' },
+            { id: 4, href: '#', content: 'Tv' },
+            { id: 5, href: '#', content: 'Games' },
+            { id: 6, href: '#', content: 'Videos' },
+            { id: 7, href: '#', content: 'News' }
+        ]
+
+    },
+    {
+        id: 2,
+        className: 'dc',
+        title: 'DC',
+        links: [
+            { id: 1, href: '#', content: 'Term of Use' },
+            { id: 2, href: '#', content: 'Privacy policy (New)' },
+            { id: 3, href: '#', content: 'Ad Choises' },
+            { id: 4, href: '#', content: 'Advertising' },
+            { id: 5, href: '#', content: 'Jobs' },
+            { id: 6, href: '#', content: 'Subscriptions' },
+            { id: 7, href: '#', content: 'Talent Workshops' },
+            { id: 8, href: '#', content: 'CPSC Certificates' },
+            { id: 9, href: '#', content: 'Ratings' },
+            { id: 10, href: '#', content: 'Shop Help' },
+            { id: 11, href: '#', content: 'Contact Us' }
+        ]
+
+    },
+    {
+        id: 3,
+        className: 'sites',
+        title: 'Sites',
+        links: [
+            { id: 1, href: '#', content: 'DC' },
+            { id: 2, href: '#', content: 'MAD Magazine' },
+            { id: 3, href: '#', content: 'DC Kids' },
+            { id: 4, href: '#', content: 'DC Universe' },
+            { id: 5, href: '#', content: 'DC Power Visa' }
+        ]
+
+    },
+    {
+        id: 4,
+        className: 'shop',
+        title: 'Shop',
+        links: [
+            { id: 1, href: '#', content: 'Shop DC' },
+            { id: 2, href: '#', content: 'Shop DC Collectibles' }
+        ]
+    }
+]
