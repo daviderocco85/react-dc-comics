5592ddddc37141f4f40aa1f99b71d8bf4eb78eae
daviderocco85 <roccodavide85@gmail.com>
2026-07-07 12:31:49 +0200

    refactor: update Main.jsx structure and improve component layout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,22 @@
 import './Main.css'
+import comics from "../data/comics.js";
+const Jumbotron = () => <div className="jumbotron"></div>
+
 
 export const Main = () => (
     <main>
-        {'-->'}
-        Content goes here
-        {'<--'}
+        <Jumbotron />
+        <div className='container'>
+            <h1>Current series</h1>
+            <div className="cards-grid">
+                {comics.map(comic => (
+                    <div key={comic.id} className='card-items'>
+                        <img src={comic.thumb} alt={comic.title} />
+                        <h2>{comic.title}</h2>
+                    </div>
+                ))}
+            </div>
+            <button className="load-more">LOAD MORE</button>
+        </div>
     </main>
 );
