ad03b01e0bd3b0bf6cd456f712621c6eaa51b040
daviderocco85 <roccodavide85@gmail.com>
2026-07-07 13:52:10 +0200

    feat: add blueBarLinks.js data source for BlueBar component

---
+++ src/data/blueBarLinks.js
@@ -0,0 +1,33 @@
+export const blueBarlinks = [
+    {
+        id: 1,
+        href: '#',
+        content: 'DIGITAL COMICS',
+        src: "img/buy-comics-digital-comics.png"
+    },
+    {
+        id: 2,
+        href: '#',
+        content: 'DC MERCHANDISE',
+        src: "img/buy-comics-merchandise.png"
+    },
+    {
+        id: 3,
+        href: '#',
+        content: 'SUBSCRIPTIONS',
+        src: "img/buy-comics-subscriptions.png"
+    },
+    {
+        id: 4,
+        href: '#',
+        content: 'COMIC SHOP LOCATOR',
+        src: "img/buy-comics-shop-locator.png"
+    },
+    {
+        id: 5,
+        href: '#',
+        content: 'DC POWER VISA',
+        src: "img/buy-dc-power-visa.svg"
+    },
+
+]
