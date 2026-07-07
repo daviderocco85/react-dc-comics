494f7c7bf0645c0389b17fd6d49b12e1cbfae6d4
daviderocco85 <roccodavide85@gmail.com>
2026-07-07 13:59:19 +0200

    refactor: update Footer.jsx to use external footerTopLinks data source

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,70 +1,6 @@
 import './Footer.css'
+import { footerTopLinks } from "../data/footerTopLinks.js";
 const Logo = () => <img className='logo' src="img/dc-logo-bg.png" alt='Logo' />;
-
-const menus = [
-    {
-        id: 1,
-        className: 'dccomics',
-        title: 'DC Comics',
-        links: [
-            { id: 1, href: '#', content: 'Characters' },
-            { id: 2, href: '#', content: 'Comics' },
-            { id: 3, href: '#', content: 'Movies' },
-            { id: 4, href: '#', content: 'Tv' },
-            { id: 5, href: '#', content: 'Games' },
-            { id: 6, href: '#', content: 'Videos' },
-            { id: 7, href: '#', content: 'News' }
-        ]
-
-    },
-    {
-        id: 2,
-        className: 'dc',
-        title: 'DC',
-        links: [
-            { id: 1, href: '#', content: 'Term of Use' },
-            { id: 2, href: '#', content: 'Privacy policy (New)' },
-            { id: 3, href: '#', content: 'Ad Choises' },
-            { id: 4, href: '#', content: 'Advertising' },
-            { id: 5, href: '#', content: 'Jobs' },
-            { id: 6, href: '#', content: 'Subscriptions' },
-            { id: 7, href: '#', content: 'Talent Workshops' },
-            { id: 8, href: '#', content: 'CPSC Certificates' },
-            { id: 9, href: '#', content: 'Ratings' },
-            { id: 10, href: '#', content: 'Shop Help' },
-            { id: 11, href: '#', content: 'Contact Us' }
-        ]
-
-    },
-    {
-        id: 3,
-        className: 'sites',
-        title: 'Sites',
-        links: [
-            { id: 1, href: '#', content: 'DC' },
-            { id: 2, href: '#', content: 'MAD Magazine' },
-            { id: 3, href: '#', content: 'DC Kids' },
-            { id: 4, href: '#', content: 'DC Universe' },
-            { id: 5, href: '#', content: 'DC Power Visa' }
-        ]
-
-    },
-    {
-        id: 4,
-        className: 'shop',
-        title: 'Shop',
-        links: [
-            { id: 1, href: '#', content: 'Shop DC' },
-            { id: 2, href: '#', content: 'Shop DC Collectibles' }
-        ]
-    }
-]
-
-
-
-
-
-
 
 
 
@@ -93,11 +29,11 @@ export const Footer = () => (
             <div className="container">
                 <div className="footer-links">
 
-                    {menus.map(menu => (
-                        <section key={menu.id} className={menu.className}>
-                            <h2>{menu.title}</h2>
+                    {footerTopLinks.map(footerTop => (
+                        <section key={footerTop.id} className={footerTop.className}>
+                            <h2>{footerTop.title}</h2>
                             <ul>
-                                {menu.links.map(link => (
+                                {footerTop.links.map(link => (
                                     <li key={link.id}><a href={link.href}>{link.content}</a></li>
                                 ))}
                             </ul>
