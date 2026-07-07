37a558395158ccef3c8c9d0e298ccdf6b7f0afcc
daviderocco85 <roccodavide85@gmail.com>
2026-07-07 11:52:08 +0200

    refactor: rewrite Header.jsx using map iteration for navigation links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,88 @@
 import './Header.css';
 
+const links = [
+    {
+        id: 1,
+        href: '#',
+        content: 'Characters',
+        active: false
+    },
+    {
+        id: 2,
+        href: '#',
+        content: 'Comics',
+        active: true
+    },
+    {
+        id: 3,
+        href: '#',
+        content: 'Movies',
+        active: false
+    },
+    {
+        id: 4,
+        href: '#',
+        content: 'Tv',
+        active: false
+    },
+    {
+        id: 5,
+        href: '#',
+        content: 'Games',
+        active: false
+    },
+    {
+        id: 6,
+        href: '#',
+        content: 'Collectibles',
+        active: false
+    },
+    {
+        id: 7,
+        href: '#',
+        content: 'Videos',
+        active: false
+    },
+    {
+        id: 8,
+        href: '#',
+        content: 'Fans',
+        active: false
+    },
+    {
+        id: 9,
+        href: '#',
+        content: 'News',
+        active: false
+    },
+    {
+        id: 10,
+        href: '#',
+        content: 'Shop',
+        active: false
+    },
+]
+
+
+
+
+
+
 const Logo = () => <img className='logo' src="img/dc-logo.png" alt='Logo' />
 
 const Nav = () => (
     <nav>
         <ul>
-            <li><a href="#">Characters</a></li>
-            <li><a href="#">Comics</a></li>
-            <li><a href="#">Movies</a></li>
-            <li><a href="#">Tv</a></li>
-            <li><a href="#">Games</a></li>
-            <li><a href="#">Collectibles</a></li>
-            <li><a href="#">Videos</a></li>
-            <li><a href="#">Fans</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">Shop</a></li>
+            {links.map(link => (
+                <li key={link.id}>
+                    <a
+                        href={link.href}
+                        className={link.active ? 'active' : undefined}
+                    >
+                        {link.content}
+                    </a>
+                </li>
+            ))}
         </ul>
     </nav>
 );
