a0e8b24b47d04283c0ed1d44357676e5df3527a6
daviderocco85 <roccodavide85@gmail.com>
2026-07-07 17:12:24 +0200

    feat: add footerBottomNavLinks.js data source for Footer component

---
+++ src/data/footerBottomNavLinks.js
@@ -0,0 +1,33 @@
+export const footerBottomNavLinks = [
+    {
+        id: 1,
+        href: '#',
+        alt: 'Facebook',
+        src: "img/footer-facebook.png"
+    },
+    {
+        id: 2,
+        href: '#',
+        alt: 'Twitter',
+        src: "img/footer-twitter.png"
+    },
+    {
+        id: 3,
+        href: '#',
+        alt: 'Youtube',
+        src: "img/footer-youtube.png"
+    },
+    {
+        id: 4,
+        href: '#',
+        alt: 'Pinterest',
+        src: "img/footer-pinterest.png"
+    },
+    {
+        id: 5,
+        href: '#',
+        alt: 'Periscope',
+        src: "img/footer-periscope.png"
+    },
+
+]
