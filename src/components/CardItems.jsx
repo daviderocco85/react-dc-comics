56461bca0dff2dce3d86c9e483d782c54d4659ba
daviderocco85 <roccodavide85@gmail.com>
2026-07-08 21:48:04 +0200

    feat: add CardItems component for comic card rendering

---
+++ src/components/CardItems.jsx
@@ -0,0 +1,9 @@
+import './CardItems.css';
+
+
+export const CardItems = ((props) =>
+    <div className="card-items">
+        <img src={props.thumb} alt={props.title} />
+        <h2>{props.title}</h2>
+    </div>
+); 
