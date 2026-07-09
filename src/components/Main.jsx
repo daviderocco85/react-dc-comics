d4a2c438ea3906e4e73a1e448e615c3654c86eac
daviderocco85 <roccodavide85@gmail.com>
2026-07-09 16:53:55 +0200

    refactor: Centralize Main data in App.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,20 @@
 import './Main.css'
-import comics from "../data/comics.js";
 import { CardItems } from "./CardItems.jsx"
 
 const Jumbotron = () => <div className="jumbotron"></div>
 
 
-export const Main = () => (
+
+
+export const Main = props => (
     <main>
         <Jumbotron />
         <div className='container'>
             <h1>Current series</h1>
             <div className="cards-grid">
-                {comics.map(comic => (
-                    <CardItems key={comic.id} thumb={comic.thumb} title={comic.title} />
-                ))}
+                {props.comics.map(comic => <CardItems key={comic.id} thumb={comic.thumb} title={comic.title} />)}
             </div>
             <button className="load-more">LOAD MORE</button>
         </div>
     </main>
 );
-
