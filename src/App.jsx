fc14ba7c7cab161ff47463b5e8c96cf4833452b5
daviderocco85 <roccodavide85@gmail.com>
2026-07-09 16:42:15 +0200

    refactor: centralize application data in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import './App.css';
+// componenti
 import { Header } from './components/Header';
 import { Main } from './components/Main';
 import { BlueBar } from './components/BlueBar';
 import { Footer } from './components/Footer';
 
+//dati privatizzati
+import { blueBarLinks } from "./data/blueBarLinks.js";
+import { footerTopLinks } from './data/footerTopLinks.js';
+import { footerBottomNavLinks } from './data/footerBottomNavLinks.js';
+import { headerNavLinks } from './data/headerNavLinks.js';
+import comics from './data/comics.js';
 
 const App = () => {
 
 
   return (
     <>
-      <Header />
-      <Main />
-      <BlueBar />
-      <Footer />
+      <Header linksNav={headerNavLinks} />
+      <Main comics={comics} />
+      <BlueBar links={blueBarLinks} />
+      <Footer linksTop={footerTopLinks} linksBottom={footerBottomNavLinks} />
 
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,44 @@
 import './Header.css';
-import { headerNavLinks } from "../data/headerNavLinks.js";
+
 
 
 const Logo = () => <img className='logo' src="img/dc-logo.png" alt='Logo' />
 
-const Nav = () => (
-    <nav>
-        <ul>
-            {headerNavLinks.map(headerNavLink => (
-                <li key={headerNavLink.id}>
-                    <a
-                        href={headerNavLink.href}
-                        className={headerNavLink.active ? 'active' : undefined}
-                    >
-                        {headerNavLink.content}
-                    </a>
-                </li>
-            ))}
-        </ul>
-    </nav>
+
+
+const NavLink = props => (
+
+    <li>
+        <a
+            href={props.link.href}
+            className={props.link.active ? 'active' : undefined}
+        >
+            {props.link.content}
+        </a>
+    </li>
+
 );
 
-export const Header = () => (
+
+
+
+export const Header = props => (
     <header>
         <div className="container">
             <Logo />
-            <Nav />
+            <nav>
+                <ul>
+                    {props.linksNav.map(link => <NavLink key={link.id} link={link} />)}
+                </ul>
+            </nav>
         </div>
     </header>
 
-
 );
+
+
+
+
+
+
+
